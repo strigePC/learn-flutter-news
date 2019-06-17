1308e5badad8872275231c50b299a66c6c1ecac8
Anuar Otynshin <strigeandro@gmail.com>
2019-06-17 12:55:13 +0600

    pass id to details screen, add comments bloc

--- lib/src/blocs/stories_provider.dart
+++ lib/src/blocs/stories_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'stories_bloc.dart';
 export 'stories_bloc.dart';
 
--- lib/src/screens/news_detail.dart
+++ lib/src/screens/news_detail.dart
@@ -1,9 +1,18 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class NewsDetail extends StatelessWidget {
+  NewsDetail({this.itemId});
+
+  final int itemId;
+
   @override
   Widget build(BuildContext context) {
-    return Text('News details');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News Detail'),
+      ),
+      body: Text('Details Screen: $itemId'),
+    );
   }
 
 }
